2322e127ed5d00ece7c2e5a55efb5bf1be6e4e6c
SunnyGitGit <sunyoung4757@gmail.com>
2025-02-19 15:09:41 +0000

    added vote on an article

--- src/components/ArticleDetails.jsx
+++ src/components/ArticleDetails.jsx
@@ -1,10 +1,12 @@
-import { getArticleById } from "../assets/api";
+import { getArticleById, voteOnArticle } from "../assets/api";
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
 export default function ArticleDetails() {
     const { articleId } = useParams();
     const [article, setArticle] = useState({});
+    const [votes, setVotes] = useState(0);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
       getArticleById(articleId).then((article) => {
@@ -15,8 +17,18 @@ export default function ArticleDetails() {
           year: 'numeric',
         });
         setArticle({ ...article, formattedDate });
+        setVotes(article.votes);
       });
     }, [articleId]); 
+
+    const handleVote = (change) => {
+      setVotes((currentVotes) => currentVotes + change);
+      setError(null);
+      voteOnArticle(articleId, change).catch((err) => {
+        setVotes((currentVotes) => currentVotes - change);
+        setError("Failed to update votes");
+      });
+    }
 
     return (
         <div className="article-details">
@@ -26,10 +38,16 @@ export default function ArticleDetails() {
             <img className="article-img-url" src={"https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?w=700&h=700"} alt="Article image" />
             <div className="vote-icon-container">
               <div className="vote-icons">
-                  <img src="/images/upvote_icon.png" alt="Upvote Icon"/>
-                  <img src="/images/downvote_icon.png" alt="Downvote Icon"/>
+                  <img 
+                  src="/images/upvote_icon.png" 
+                  alt="Upvote Icon" 
+                  onClick={() => handleVote(1)}/>
+                  <img 
+                  src="/images/downvote_icon.png" 
+                  alt="Downvote Icon" 
+                  onClick={() => handleVote(-1)}/>
               </div>
-              <p className="vote-count">Votes: {article.votes}</p>
+              <p className="vote-count">Votes: {votes}</p>
             </div>
             <p>Topic: {article.topic}</p>
             <p>Description: {article.body}</p>
@@ -42,4 +60,6 @@ export default function ArticleDetails() {
         </div>
     );
 };
+
+
 
