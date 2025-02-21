ed83ec761a77f9e9575103748fbecb65968e829e
SunnyGitGit <sunyoung4757@gmail.com>
2025-02-21 10:22:15 +0000

    added view-articles-by-topic

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,27 +1,60 @@
 import { useEffect, useState } from "react";
-import { useResolvedPath } from "react-router-dom";
-import { getArticles } from "../assets/api";
+import { useSearchParams } from "react-router-dom";
+import { getArticles, getArticlesByTopic, getTopics } from "../assets/api";
 import ArticleCard from "./ArticleCard";
 
 export default function ArticleList() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchParams] = useSearchParams();
+  const topicName = searchParams.get("topic");
+  const [topics, setTopics] = useState([]);
+  const [selectedTopic, setSelectedTopic] = useState(topicName || "");
+
+  useEffect(() => {
+    getTopics()
+      .then((topics) => {
+        setTopics(topics);
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+  }, []);
 
   useEffect(() => {
     setLoading(true);
     setError(null);
 
-    getArticles()
-      .then((articles) => {
-        setArticles(articles);
-        setLoading(false);
-    })
-    .catch((error) => {
-      setError(error.message);
-      setLoading(false);
-    })
-  }, []);
+    const fetchArticles = () => {
+      if (selectedTopic) {
+        getArticlesByTopic(selectedTopic)
+          .then((filteredArticles) => {
+            setArticles(filteredArticles);
+            setLoading(false);
+          })
+          .catch((error) => {
+            setError(error.message);
+            setLoading(false);
+          });
+      } else {
+        getArticles()
+          .then((articles) => {
+            setArticles(articles);
+            setLoading(false);
+          })
+          .catch((error) => {
+            setError(error.message);
+            setLoading(false);
+          });
+      };
+    };
+    fetchArticles();
+  }, [selectedTopic]);
+
+  const handleTopicChange = (event => {
+    setSelectedTopic(event.target.value);
+  });
 
   if (loading) return <p>Loading articles...</p>;
   if (error) return <p>Error: {error}</p>;
@@ -29,6 +62,18 @@ export default function ArticleList() {
   return (
     <div className="articlelist-card-container">
         <h2>Articles</h2>
+        <div className="topic-selection">
+            <label htmlFor="topic-select">Search Articles by Topic: </label>
+            <select id="topic-select" value={selectedTopic} onChange={handleTopicChange}>
+              <option value="">All topics</option>
+              {topics.map((topic) => (
+                <option key={topic.slug} value={topic.slug}>
+                  {topic.slug}
+                </option>
+              ))}
+            </select>
+        </div>
+        <br />
         <div className="card-grid">
             {articles.map((article) => (
             <div key={article.article_id}>
